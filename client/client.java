09366ce3a0de57cd7fd9c661514c88f98b7c6ec2
cealcorn <cassidyealcorn@gmail.com>
2022-10-19 12:45:01 -0400

    client code cont

--- client/client.java
+++ client/client.java
@@ -8,39 +8,110 @@ import java.util.Scanner;
 
 public class client {
 
+    // TO DO: implement the client part that will work with the server part and DOWNLOAD a file from the server
+    //          implement the delete command
+    //          follow server.java to write the code for this .java
+
     public static void main(String[] args) throws IOException {
 
         if (args.length != 2) {
             System.out.println(
-                    "Usage: ClientTCP <server_ip> <server_port>");
+                    "Usage: java client <server_ip> <server_port>"
+            );
             return;
         }
 
         int serverPort = Integer.parseInt(args[1]);
         String serverIP = args[0];
 
-        System.out.println(
-                "Press a key, then press enter.");
-        Scanner keyboard = new Scanner(System.in);
-        char c = keyboard.nextLine().charAt(0);
+        char command;
 
-        byte[] b = new byte[1];
-        b[0] = (byte) c;
-        ByteBuffer buffer = ByteBuffer.wrap(b);
+        do {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println(
+                    "Enter a command:\n" +
+                            "Upload - U\n" +
+                            "Download - D\n" +
+                            "List - L\n" +
+                            "Rename - R\n" +
+                            "Move - M\n" +
+                            "Help - H\n" +
+                            "Quit - Q\n"
+            );
+            command = keyboard.nextLine().toUpperCase().charAt(0);
 
-        SocketChannel sc = SocketChannel.open();
-        sc.connect(new InetSocketAddress(serverIP, serverPort));
-        sc.write(buffer);
+            switch (command) {
+                // CASE 'U' upload
 
-        ByteBuffer rb = ByteBuffer.allocate(1);
-        sc.read(rb);
-        rb.rewind();
-        System.out.println(
-                "Message from server: " + (char) rb.get());
+                // CASE 'D' download
+                case 'D':
+                    System.out.println(
+                            "Enter name of the file to donwload: "
+                    );
+                    String fileName = keyboard.nextLine();
+                    ByteBuffer buffer = ByteBuffer.wrap(("G" + fileName).getBytes());
+                    SocketChannel channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(serverIP, serverPort));
+                    channel.write(buffer);
 
-        rb.flip();
-        sc.write(buffer);
-        sc.close();
+                    // shut down output on client side after client is done sending to server
+                    channel.shutdownOutput();
+
+                    // receive server reply code
+                    if (getServerCode(channel) != 'C') { // replaced F from canvas code
+                        System.out.println(
+                                "Server failed to serve the request."
+                        );
+                    } else {
+                        System.out.println(
+                                "The request was accepted."
+                        );
+                        Files.createDirectories(Paths.get("./downloaded"));
+
+                        BufferedWriter bw = new BufferedWriter(new FileWriter(
+                                "./downloaded/" + fileName, true));
+                        ByteBuffer data = ByteBuffer.allocate(1024);
+                        int bytesRead;
+
+                        while ((bytesRead = channel.read(data)) != -1) {
+                            data.flip();
+                            byte[] a = new byte[bytesRead];
+                            data.get(a);
+                            String serverMessage = new String(a);
+                            bw.write(serverMessage);
+                            data.clear();
+                        }
+                        bw.close();
+                    }
+                    channel.close();
+                    break;
+
+                // CASE 'L' list
+
+                // CASE 'R' rename
+
+                // CASE 'M' move
+
+                // CASE 'H' help
+
+            }
+        } while (command != 'Q');
     }
 
+    private static char getServerCode(SocketChannel channel) throws IOException{
+        ByteBuffer buffer = ByteBuffer.allocate(1);
+        int bytesToRead = 1;
+
+        //make sure we read the entire server reply
+        while((bytesToRead -= channel.read(buffer)) > 0);
+
+        buffer.flip();
+        byte[] a = new byte[1];
+        buffer.get(a);
+        char serverReplyCode = new String(a).charAt(0);
+
+        //System.out.println(serverReplyCode);
+
+        return serverReplyCode;
+    }
 }
