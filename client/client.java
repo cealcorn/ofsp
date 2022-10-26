21e80a27d8e03eec35fa76861b372262c2b949ed
cealcorn <cassidyealcorn@gmail.com>
2022-10-26 02:46:28 -0400

    - fixed a stupid spelling error that was def causing problems - followed transaction/comm scheme for 'upload' case
    26/22 2:46

--- client/client.java
+++ client/client.java
@@ -8,10 +8,14 @@ import java.util.Scanner;
 
 public class client {
 
-    // TO DO: implement the client part that will work with the server part and DOWNLOAD a file from the server
-    //          implement the delete command
-    //          follow server.java to write the code for this .java
-    // 8080
+    // GENERAL UPDATES:
+        // Fixed System.out.println("Error type: " + getServerCode(channel).substring(1); where "channel" was spelled ..
+            // "chanel" ...oops
+        // Followed transaction.txt stuff for 'upload' in case below (untested currently)
+
+    // TO DO:
+        // Follow same transaction scheme for Download (if its not already fine)
+        // Complete 4 (ideally five for that sweet sweet extra cred) other client-side cases
 
     public static void main(String[] args) throws IOException {
 
@@ -42,42 +46,52 @@ public class client {
             command = keyboard.nextLine().toUpperCase().charAt(0);
 
             switch (command) {
-                // CASE 'U' upload
+                // CASE 'U' UPLOAD
                 case 'U':
-                    System.out.println(
-                            "Enter path of the file to upload: "
-                    );
-                    String filePath = keyboard.nextLine();
-                    ByteBuffer buffer = ByteBuffer.wrap(("U" + filePath).getBytes());
                     SocketChannel channel = SocketChannel.open();
                     channel.connect(new InetSocketAddress(serverIP, serverPort));
-                    channel.write(buffer);
+                    channel.write(command);                                 // Client sends "U"
 
-                    if (getServerCode(channel) != 'C') { // replaced F from canvas code
+                    if (getServerCode(channel) != 'C') {                    // alt. if getServerCode(channel) = 'E'
                         System.out.println(
-                                "Server failed to serve the request."
+                                "Server failed to serve the request."       // Server replies with "E" and the error ..
+                        );                                                  // message directly after if error; ..
+                        System.out.println(                                 // EX: ECommand Not Found
+                                "Error type: " + getServerCode(channel).substring(1)
                         );
-                        System.out.println(
-                                "Error type: " + getServerCode(chanel).substring(1)
-                        );
-                    } else {
+                    } else {                                                // Server replies with "C" when ready
                         System.out.println(
                                 "The request was accepted."
                         );
 
                         try (Socket socket = new Socket("localhost", 8080)) { // local host?
-                            dataInputStream = new DataInputStream(socket.getInputStream());
-                            dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-                            sendFile(filePath);
-
-                            dataInputStream.close();
-                        } catch (Exception e){
+                            System.out.println(
+                                    "Enter path of the file to upload: "
+                            );
+                            String filePath = keyboard.nextLine();          // Client sends file with null character ..
+                            ByteBuffer buffer = ByteBuffer.wrap(("U" + null + filePath).getBytes());
+                            channel.write(buffer);                          // seperating file name and file data
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
+
+                        if (getServerCode(channel) != 'C') {                // Server replies with "C" if successful
+                            System.out.println(
+                                    "Server was unable to process upload."
+                            );
+                            System.out.println(                             // Server replies with "E" and the error ..
+                                    "Error type: " + getServerCode(channel).substring(1)
+                            );                                              //  message directly after if error; ..
+                        } else {                                            // EX: EWrite not permitted, insufficient ..
+                            System.out.println(                             // permissions
+                                    "Upload request was accepted."
+                            );
+                        }
+                        channel.close();
+                        break;
                     }
 
-                // CASE 'D' download
+                // CASE 'D' DOWNLOAD
                 case 'D':
                     System.out.println(
                             "Enter name of the file to download: "
@@ -92,13 +106,16 @@ public class client {
                     channel.shutdownOutput();
 
                     // receive server reply code
-                    if (getServerCode(channel) != 'C') { // replaced F from canvas code
+                    if (getServerCode(channel) != 'C') { // alt. if getServerCode(channel) = 'E'
                         System.out.println(
                                 "Server failed to serve the request."
                         );
+                        System.out.println(
+                                "Error type: " + getServerCode(channel).substring(1)
+                        );
                     } else {
                         System.out.println(
-                                "The request was accepted."
+                                "Download  request was accepted."
                         );
                         Files.createDirectories(Paths.get("./downloaded"));
 
@@ -149,17 +166,17 @@ public class client {
         return serverReplyCode;
     }
 
-    private static void sendFile(String path) throws Exception {
-        int bytes = 0;
-        File file = new File(path);
-        FileInputStream fileInputStream = new FileInputStream(file);
-
-        dataOutputStream.writeLong(file.length());
-        byte[] buffer = new byte[4*1024];
-        while ((bytes=fileInputStream.read(buffer)) != -1) {
-            dataOutputStream.write(buffer, 0, bytes);
-            dataOutputStream.flush();
-        }
-        fileInputStream.close();
-    }
+//    private static void sendFile(String path) throws Exception {
+//        int bytes = 0;
+//        File file = new File(path);
+//        FileInputStream fileInputStream = new FileInputStream(file);
+//
+//        dataOutputStream.writeLong(file.length());
+//        byte[] buffer = new byte[4*1024];
+//        while ((bytes=fileInputStream.read(buffer)) != -1) {
+//            dataOutputStream.write(buffer, 0, bytes);
+//            dataOutputStream.flush();
+//        }
+//        fileInputStream.close();
+//    }
 }
