91f874115dc9cbf2475a2e606779ff44f93cc8e3
nickquednow <56178732+nickquednow@users.noreply.github.com>
2022-10-27 21:26:39 -0400

    refactor argument code and add server continous connection

--- server/server2.java
+++ server/server2.java
@@ -27,10 +27,9 @@ public class server2 {
         System.out.println("USAGE: java ServerTCP.java listening_port");
     }
 
-    public static void main(String[] args) {
+    private static int verify_arguments(String[] args) throws InvalidPortNumber, InvalidArgumentLength {
         if (args.length != 1) {
-            print_usage();
-            return;
+            throw new server2.InvalidArgumentLength("Invalid number of arguments");
         }
 
         int port;
@@ -38,15 +37,30 @@ public class server2 {
             port = Integer.parseInt(args[0]);
         } catch (NumberFormatException ex) {
             System.out.println("The port number could not be parsed. please input a valid integer between 1 and 65535");
-            print_usage();
-            return;
+            throw new server2.InvalidPortNumber("Port not an integer");
         }
 
         if (port > 65535) {
             System.out.println("The port is above the range, please input a port between 1 and 65535");
-            return;
+            throw new server2.InvalidPortNumber("Port above range.");
         } else if (port < 1) {
             System.out.println("The port is below the range, please input a port between 1 and 65535");
+            throw new server2.InvalidPortNumber("Port below range.");
+        }
+        return port;
+    }
+
+    public static void main(String[] args) {
+        int port;
+        try {
+            port = verify_arguments(args);
+        } catch (InvalidArgumentLength e) {
+            System.out.println(e.getMessage());
+            print_usage();
+            return;
+        } catch (InvalidPortNumber e) {
+            System.out.println(e.getMessage());
+            print_usage();
             return;
         }
 
@@ -56,135 +70,128 @@ public class server2 {
             System.out.println("Unable to bind to port " + String.valueOf(port) + ". Error: " + e.getMessage());
             return;
         }
-
-        try {
-            clientSocket = serveSocket.accept();
-        } catch (IOException e) {
-            System.out.println("Unable to create client connection");
-            return;
-        }
-
-        // adds ability to switch to different parts of the code
-        boolean recievedCommand = false;
-        boolean recievedPayload = false;
-        try {
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        } catch (IOException e) {
-            System.out.println("could not create the buffer");
-        }
-
-        String currentInput;
-        String humanReadableCommand;
-        char command = 'z';
-        String payload;
-        Boolean isError = false;
-        try {
-            while ((currentInput = in.readLine()) != null) {
-                if(isError){
-                    break;
-                }
-                if (!recievedCommand) {
-                    command = currentInput.charAt(0);
-                    switch (command) {
-                        case 'D':
-                            humanReadableCommand = "Download";
-                            break;
-                        case 'U':
-                            humanReadableCommand = "Upload";
-                            break;
-                        case 'L':
-                            humanReadableCommand = "List";
-                            break;
-                        case 'R':
-                            humanReadableCommand = "Rename";
-                            break;
-                        case 'M':
-                            humanReadableCommand = "Move";
-                            break;
-                        case 'H':
-                            humanReadableCommand = "Help";
-                            break;
-                        case 'T':
-                            humanReadableCommand = "Delete";
-                            break;
-                        default:
-                            humanReadableCommand = "Unknown";
-                            break;
-                    }
-                    System.out.println("Command recieved from client: " + humanReadableCommand);
-                    recievedCommand = true;
-                    if (humanReadableCommand == "Unknown") {
-                        recievedCommand = false;
-                        throwError("Unknown Command. Please retry");
-                    }
-                    continue; // go wait for the next line to come in
-                } else if (!recievedPayload) {
-                    switch (command) {
-                        case 'D':
-                            payload = currentInput.substring(1);
-                            File downloadFile;
-                            try {
-                                downloadFile = checkFileAvailability(payload);
-                            } catch (BadPermissionsException e) {
-                                System.out.println("Error from client download command: " + e.getMessage());
-                                break;
-                            } catch (IncorrectFileNameException e) {
-                                System.out.println("Error from client download command: " + e.getMessage());
-                                break;
-                            } catch (IsDirectoryException e) {
-                                System.out.println("Error from client download command: " + e.getMessage());
-                                break;
-                            }
-                            try{
-                                sendFile(downloadFile);
-                            } catch(EmptyFileException e){
-                                throwError("File is empty, nothing to send");
-                                System.out.println("File is Empty for [" + downloadFile.getName() + "].");
-                            }
-                            break;
-                        case 'U':
-                            humanReadableCommand = "Upload";
-                            break;
-                        case 'L':
-                            humanReadableCommand = "List";
-                            break;
-                        case 'R':
-                            humanReadableCommand = "Rename";
-                            break;
-                        case 'M':
-                            humanReadableCommand = "Move";
-                            break;
-                        case 'H':
-                            humanReadableCommand = "Help";
-                            break;
-                        case 'T':
-                            humanReadableCommand = "Delete";
-                            break;
-                        default:
-                            throwError(
-                                    "You were not supposed to get to this point... contact the admin to fix this error along with how you got here");
-                    }
-                }
+        while (true) { //keeps server connection open and accepts multiple clients
+            try {
+                clientSocket = serveSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Unable to create client connection");
+                return;
             }
-        } catch (IOException e) {
-            System.out.println("Unable to read from input. Error: " + e);
-        }
 
-            
-        try {
-            in.close();
-            clientSocket.close();
-        } catch (IOException e) {
-            System.out.println("Unable to close client socket. Error: " + e.getMessage());
-        }
+            // adds ability to switch to different parts of the code
+            boolean recievedCommand = false;
+            boolean recievedPayload = false;
+            try {
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            } catch (IOException e) {
+                System.out.println("could not create the buffer");
+            }
 
-        try {
-            out.close();
-            serveSocket.close();
-        } catch (IOException e) {
-            System.out.println("Unable to close server socket. Error: " + e.getMessage());
+            String currentInput;
+            String humanReadableCommand;
+            char command = 'z';
+            String payload;
+            Boolean isError = false;
+            try {
+                while ((currentInput = in.readLine()) != null) {
+                    if (isError) {
+                        break;
+                    }
+                    // verify that the command is valid
+                    if (!recievedCommand) {
+                        command = currentInput.charAt(0);
+                        switch (command) {
+                            case 'D':
+                                humanReadableCommand = "Download";
+                                break;
+                            case 'U':
+                                humanReadableCommand = "Upload";
+                                break;
+                            case 'L':
+                                humanReadableCommand = "List";
+                                break;
+                            case 'R':
+                                humanReadableCommand = "Rename";
+                                break;
+                            case 'M':
+                                humanReadableCommand = "Move";
+                                break;
+                            case 'H':
+                                humanReadableCommand = "Help";
+                                break;
+                            case 'T':
+                                humanReadableCommand = "Delete";
+                                break;
+                            default:
+                                humanReadableCommand = "Unknown";
+                                break;
+                        }
+                        System.out.println("Command recieved from client: " + humanReadableCommand);
+                        recievedCommand = true;
+                        if (humanReadableCommand == "Unknown") {
+                            recievedCommand = false;
+                            throwError("Unknown Command. Please retry");
+                        }
+                        continue; // go wait for the next line to come in
+                    } else if (!recievedPayload) {
+                        switch (command) {
+                            case 'D':
+                                payload = currentInput.substring(1);
+                                File downloadFile;
+                                try {
+                                    downloadFile = checkFileAvailability(payload);
+                                } catch (BadPermissionsException e) {
+                                    System.out.println("Error from client download command: " + e.getMessage());
+                                    break;
+                                } catch (IncorrectFileNameException e) {
+                                    System.out.println("Error from client download command: " + e.getMessage());
+                                    break;
+                                } catch (IsDirectoryException e) {
+                                    System.out.println("Error from client download command: " + e.getMessage());
+                                    break;
+                                }
+                                try {
+                                    sendFile(downloadFile);
+                                } catch (EmptyFileException e) {
+                                    throwError("File is empty, nothing to send");
+                                    System.out.println("File is Empty for [" + downloadFile.getName() + "].");
+                                }
+                                break;
+                            case 'U':
+                                humanReadableCommand = "Upload";
+                                break;
+                            case 'L':
+                                humanReadableCommand = "List";
+                                break;
+                            case 'R':
+                                humanReadableCommand = "Rename";
+                                break;
+                            case 'M':
+                                humanReadableCommand = "Move";
+                                break;
+                            case 'H':
+                                humanReadableCommand = "Help";
+                                break;
+                            case 'T':
+                                humanReadableCommand = "Delete";
+                                break;
+                            default:
+                                throwError(
+                                        "You were not supposed to get to this point... contact the admin to fix this error along with how you got here");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Unable to read from input. Error: " + e);
+            }
+
+            try {
+                in.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("Unable to close client socket. Error: " + e.getMessage());
+            }
         }
-        main(args);
     }
 
     // sends the error message over the TCP socket
@@ -192,24 +199,24 @@ public class server2 {
         out.println("E" + error);
     }
 
-    private static void sendFile(File file) throws EmptyFileException{
+    private static void sendFile(File file) throws EmptyFileException {
         String outputBuffer = file.toString();
-        if(!outputBuffer.isEmpty()){
+        if (!outputBuffer.isEmpty()) {
             out.println("C" + outputBuffer);
         } else {
             throw new server2.EmptyFileException("File is empty");
         }
     }
 
-    // verifies that the file is a file and the server (aka user on the computer) has suffiecient
+    // verifies that the file is a file and the server (aka user on the computer)
+    // has suffiecient
     // permissions to read it
     private static File checkFileAvailability(String name)
             throws BadPermissionsException, IncorrectFileNameException, IsDirectoryException {
         File file = new File("data/" + name);
         if (!file.exists()) {
             throwError("file does not exist");
-            throw new server2.IncorrectFileNameException(
-                    "This file [" + name.toString() + "] does not exist in the server data directory");
+            throw new server2.IncorrectFileNameException(IncorrectFileNameException.BadFileName, name);
         }
         if (file.isDirectory()) {
             throwError("file is a directory");
@@ -219,17 +226,25 @@ public class server2 {
             return file;
         } else {
             throwError("Server error occured, contact server admin if this keeps occuring; CODE: READ DENIED");
-            throw new server2.BadPermissionsException("this file is not readable");
+            throw new server2.BadPermissionsException(BadPermissionsException.BadRead);
         }
     }
 
     private static class BadPermissionsException extends Exception {
+        public static final String BadRead = "File is unreadable";
+
         public BadPermissionsException(String errorMessage) {
             super(errorMessage);
         }
     }
 
     private static class IncorrectFileNameException extends Exception {
+        public static final String BadFileName = "File not found: ";
+
+        public IncorrectFileNameException(String errorMessage, String fileName) {
+            super(errorMessage + fileName);
+        }
+
         public IncorrectFileNameException(String errorMessage) {
             super(errorMessage);
         }
@@ -241,8 +256,20 @@ public class server2 {
         }
     }
 
-    private static class EmptyFileException extends Exception{
+    private static class EmptyFileException extends Exception {
         public EmptyFileException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    private static class InvalidPortNumber extends Exception {
+        public InvalidPortNumber(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    private static class InvalidArgumentLength extends Exception {
+        public InvalidArgumentLength(String errorMessage) {
             super(errorMessage);
         }
     }
